0eee1bdf09a5104008482b5bd5e369416a3f805a
breno.jacubovski@owshq.com <breno.jacubovski@picpay.com>
2022-12-26 18:49:58 -0300

    adicionando configuracoes de deployer

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_sub_numbers(self):
+        res = calc.sub(6, 5)
+        self.assertEqual(res, 1)
